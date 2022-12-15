b787609068c2bbdcced951df83739780b1f5d9cf
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-12-15 16:03:30 +0800

    update

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,13 +1,11 @@
 const Header = () => {
   return (
     <>
-      <div className="text-center sm:mx-auto sm:w-full sm:max-w-md space-y-1">
-        <h2 className="mt-6  text-3xl font-extrabold text-gray-900">
+      <div className="space-y-1 text-center sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6  text-3xl font-extrabold text-indigo-600">
           Exchange Rate
         </h2>
-        <p className="font-medium text-indigo-600 hover:text-indigo-500">
-          Know what&apos;s ahead
-        </p>
+        <p className="font-medium text-gray-700">Know what&apos;s ahead</p>
       </div>
     </>
   );
--- src/components/Core/CurrencyConverter.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CurrencyConverter = () => {
-  return (
-    <div>CurrencyConverter</div>
-  )
-}
-
-export default CurrencyConverter

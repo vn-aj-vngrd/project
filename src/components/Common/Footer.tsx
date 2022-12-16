791958e0b89519edda6fd57f461b2c8c5dc84788
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-12-16 18:09:57 +0800

    update

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="text-sm text-gray-700">Oliver Juntilla</footer>;
+};
+
+export default Footer;
--- src/components/Common/Layout.tsx
+++ src/components/Common/Layout.tsx
@@ -1,5 +1,6 @@
-import Header from "./Header";
 import Meta from "./Meta";
+import Header from "./Header";
+import Footer from "./Footer";
 
 type Props = {
   children: React.ReactNode;
@@ -9,9 +10,10 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Meta />
-      <div className="mx-auto flex flex-col min-h-screen w-full max-w-7xl items-center justify-center">
+      <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col items-center justify-center">
         <Header />
         <main>{children}</main>
+        <Footer />
       </div>
     </>
   );

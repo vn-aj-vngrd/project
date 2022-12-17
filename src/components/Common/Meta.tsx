8379790b5b81039d11e49210298155141ab4217d
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-12-17 17:49:45 +0800

    update meta

--- src/components/Common/Meta.tsx
+++ src/components/Common/Meta.tsx
@@ -6,7 +6,7 @@ const Meta = () => {
       <title>Project</title>
       <meta charSet="utf-8" />
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <meta name="title" content="WatchBox" />
+      <meta name="title" content="Project" />
       <meta name="description" content="Predict what's ahead" />
       <meta name="keywords" content="Project" />
       <link rel="icon" href="/favicon.ico" />

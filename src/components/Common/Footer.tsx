e1c487d555583e32a750330e24fb0e924a4bd9d6
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-12-19 17:25:13 +0800

    update footer

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer className="text-sm text-gray-700 pb-4">Oliver Juntilla</footer>;
+  return <footer className="text-sm text-gray-700 pb-4">Forex Forecaster 2022</footer>;
 };
 
 export default Footer;
